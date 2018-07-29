947ddafa0f3da6c33e92c07f0a7d946e141b5064
Spencer Evans <spencerevans.sce@gmail.com>
2018-07-29 21:28:51 +0300

    added title

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,7 @@ export default class App extends Component {
     return (
       <div>
         <div className="grid">
+          <h1 className="grid__title">Birthday Countdown</h1>
           <div className="grid__skew-dark-one-box"></div>
           <div className="grid__skew-dark-two"></div>
           <div className="grid__skew-dark-three"></div>
