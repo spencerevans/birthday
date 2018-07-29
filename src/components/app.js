3b69549dfda9cd00ff07b2f8d8b3d203d23462d3
Spencer Evans <spencerevans.sce@gmail.com>
2018-07-29 21:10:08 +0300

    added second skew

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,10 @@ export default class App extends Component {
     return (
       <div>
         <div className="grid">
-          <div>hi</div>
-          <div>hi</div>
-          <div>hi</div>
+          <div className="grid__skew-dark-one-box"></div>
+          <div className="grid__skew-dark-two"></div>
+          <div className="grid__skew-dark-three"></div>
+          
           <div className="grid__skew-light-one"></div>
           <div className="grid__skew-light-two"></div>
           <div className="grid__skew-light-three-box"></div>
